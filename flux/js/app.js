f6a6e489b43fe8c5b4cf5cd6f1507aaf23c0d164
桐木 <tongmu.lj@alibaba-inc.com>
2015-03-27 16:50:39 +0800

    解决warnning: `Each child in an array should have a unique "key" prop`

--- flux/js/app.js
+++ flux/js/app.js
@@ -4,14 +4,17 @@ var TodoApp = require('./components/TodoApp');
 
 var todolist = [
   {
+    id: 10001,
     text: 'todo list data item 1',
     complete: false
   },
   {
+    id: 10002,
     text: 'todo list data item 2',
     complete: false
   },
   {
+    id: 10003,
     text: 'todo list data item 3',
     complete: false
   }
