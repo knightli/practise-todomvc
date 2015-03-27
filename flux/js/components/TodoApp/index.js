fedd52dc8024fd3f754a7cb53f91020de4661c18
桐木 <tongmu.lj@alibaba-inc.com>
2015-03-27 15:41:23 +0800

    脚手架和外层容器 TodoApp 空壳

---
+++ flux/js/components/TodoApp/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var render = require('./render.jsx');
+
+var TodoApp = React.createClass({
+  render: render
+});
+
+module.exports = TodoApp;
---
+++ flux/js/components/TodoApp/render.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+module.exports = function() {
+  return (
+    <div>
+      this is a todoapp
+    </div>
+  );
+}
